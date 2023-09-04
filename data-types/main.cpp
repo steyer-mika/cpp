e6f4633c5d048c9ba163d882458b5d47f3b5040d
Mika Steyer <mikasteyer9@gmail.com>
2023-09-04 19:31:20 +0200

    refactor: data types update comments feat: add string data type

--- data-types/main.cpp
+++ data-types/main.cpp
@@ -1,45 +1,53 @@
+#include <iostream>
+
 int main()
 {
     /**
-     * int - Integer
-     * size 4 bytes
-     * range {-2147483648, 2147483647}
+     * `int` - Integer
+     * size: 4 bytes
+     * range: {-2147483648, 2147483647}
      */
-    int integrer = 11;
+    int integer = 11;
 
     /**
-     * float - Floating Point
-     * size 4 bytes
-     * range {1.175494351E-38, 3.402823466E+38}
+     * `float` - Floating Point
+     * size: 4 bytes
+     * range: {1.175494351E-38, 3.402823466E+38}
      */
     float floatingPoint = 64.74;
 
     /**
-     * double - double Floating Point
-     * size 8 bytes
-     * range {2.2250738585072014E-308, 1.7976931348623158E+308}
+     * `double` - Double Floating Point
+     * size: 8 bytes
+     * range: {2.2250738585072014E-308, 1.7976931348623158E+308}
      */
     double doubleFloatingPoint = 134.64534;
 
     /**
-     * char - Character
-     * size 1 bytes
+     * `char` - Character
+     * size: 1 byte
      */
     char character = 'a';
 
     /**
-     * wchar_t - Wide Character
-     * size 2 bytes
-     * is used to represent characters that require more memory to represent them
-     * like storing Hebrew character
+     * `wchar_t` - Wide Character
+     * size: 2 bytes
+     * Used to represent characters that require more memory, such as storing Hebrew characters.
      */
     wchar_t wideCharacter = L'ם';
 
     /**
-     * bool - Boolean
-     * size 1 bytes
+     * `bool` - Boolean
+     * size: 1 byte
      */
     bool boolean = true;
+
+    /**
+     * `std::string` - String
+     * namespace: std
+     * size: The size depends on the string length and implementation.
+     */
+    std::string str = "Hello World";
 
     return 0;
 }
