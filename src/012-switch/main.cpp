7f5d827fad1fd34d654f1b7a382457f3c1baf3a6
Mika Steyer <mikasteyer9@gmail.com>
2023-09-05 19:30:12 +0200

    feat: add switch

---
+++ src/012-switch/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+int main()
+{
+    int month;
+
+    std::cout << "Enter month (1-12): ";
+    std::cin >> month;
+
+    switch (month)
+    {
+    case 1:
+        std::cout << "It's January";
+        break;
+
+    case 2:
+        std::cout << "It's February";
+        break;
+
+    case 3:
+        std::cout << "It's March";
+        break;
+
+    case 4:
+        std::cout << "It's April";
+        break;
+
+    case 5:
+        std::cout << "It's May";
+        break;
+
+    case 6:
+        std::cout << "It's June";
+        break;
+
+    case 7:
+        std::cout << "It's July";
+        break;
+
+    case 8:
+        std::cout << "It's August";
+        break;
+
+    case 9:
+        std::cout << "It's September";
+        break;
+
+    case 10:
+        std::cout << "It's October";
+        break;
+
+    case 11:
+        std::cout << "It's November";
+        break;
+
+    case 12:
+        std::cout << "It's December";
+        break;
+
+    default:
+        std::cout << "Your input doesn't match a month between 1 - 12";
+        break;
+    }
+
+    return 0;
+}
