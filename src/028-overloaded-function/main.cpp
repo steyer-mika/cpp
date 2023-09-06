04590378eafbe3c576a51dbcd6ae662cfb5ebf90
Mika Steyer <mikasteyer9@gmail.com>
2023-09-06 20:12:46 +0200

    feat: add overloaded function

---
+++ src/028-overloaded-function/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void bakePizza();
+void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string topping2);
+
+int main()
+{
+    bakePizza();
+
+    bakePizza("salami");
+
+    bakePizza("salami", "pepperoni");
+
+    return 0;
+}
+
+void bakePizza()
+{
+    std::cout << "Here is your Pizza\n";
+}
+
+void bakePizza(std::string topping1)
+{
+    std::cout << "Here is your " << topping1 << " Pizza\n";
+}
+
+void bakePizza(std::string topping1, std::string topping2)
+{
+    std::cout << "Here is your " << topping1 << " and " << topping2 << " Pizza\n";
+}
